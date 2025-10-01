a9ad76aa1b0868a64ba21e75d297ea4630b2e47c
Artem <artem.dubovov@bk.ru>
2025-10-01 20:00:31 +0300

    feat: add smoth scroll

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ let objMoveToMenu = {
 
 for (let i  = 1; i <= 7; i++) {
     const btn = document.getElementById(`menuButton-${i}`);
-    section = document.getElementById(`section-${i}`);
-    let top = section.offsetTop - 120;
+
     btn.onclick = function () {
-        console.log("Click")
+        section = document.getElementById(`section-${i}`);
         window.scrollTo({
-        top: top,
+        top: section.offsetTop - 120,
         left: 0,
         behavior: "smooth"
         });
